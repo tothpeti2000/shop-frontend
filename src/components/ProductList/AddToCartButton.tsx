d4c2acc5c5960965f11f84a23d1c871d91d3f42f
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-07 21:57:34 +0100

    ProductList added

---
+++ src/components/ProductList/AddToCartButton.tsx
@@ -0,0 +1,42 @@
+import { Button, Icon, useToast } from "@chakra-ui/react";
+import React from "react";
+import { FaCartPlus } from "react-icons/fa";
+
+interface IProps {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const AddToCartButton = (props: IProps) => {
+  //const { AddItem } = useOrderItemContext();
+  const toast = useToast();
+
+  const ShowToast = () => {
+    toast({
+      title: "Item added",
+      description: "We've added the item to your cart",
+      status: "success",
+      duration: 1000,
+      isClosable: true,
+    });
+  };
+
+  const HandleClick = () => {
+    //AddItem(props.id, props.title, props.price);
+    ShowToast();
+  };
+
+  return (
+    <Button
+      colorScheme="red"
+      w="100%"
+      leftIcon={<Icon as={FaCartPlus} />}
+      onClick={HandleClick}
+    >
+      Add to cart
+    </Button>
+  );
+};
+
+export default AddToCartButton;
