8b28ba0365de8e76139f8118b7cd2d3f2bd56762
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-13 17:19:21 +0100

    Links added to CategoryCards

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -1,16 +1,15 @@
 import { Box, Text } from "@chakra-ui/layout";
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import PlaceHolder from "./PlaceHolder";
 
 const CategoryCard: FC = ({ children }) => {
   return (
     <Box flex="1" textAlign="center" p={5}>
-      {/*<Link to="/products">
+      <Link to="/products">
         <PlaceHolder />
         <Text fontSize="xl">{children}</Text>
-  </Link>*/}
-      <PlaceHolder />
-      <Text fontSize="xl">{children}</Text>
+      </Link>
     </Box>
   );
 };
