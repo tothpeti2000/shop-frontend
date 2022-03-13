f85a1f152f90f1e5614d4e6d8745ab9112e176ea
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-13 16:37:07 +0100

    Styling modified

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,10 +1,21 @@
 import { Flex, Heading } from "@chakra-ui/layout";
 import { Box } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import Fade from "react-awesome-reveal";
 import CategoryCard from "./CategoryCard";
 
+interface Category {
+  ID: number;
+  name: string;
+}
+
 const Categories = () => {
+  const [categories, setCategories] = useState<Array<Category>>([
+    { ID: 1, name: "Construction toys" },
+    { ID: 2, name: "LEGO" },
+    { ID: 3, name: "F1 LEGO" },
+  ]);
+
   return (
     <Box
       bgGradient="radial(gray.500, gray.900)"
@@ -16,10 +27,9 @@ const Categories = () => {
       <Heading>Shop categories</Heading>
       <Fade>
         <Flex p={5}>
-          <CategoryCard>Men's collection</CategoryCard>
-          <CategoryCard>Accessories</CategoryCard>
-          <CategoryCard>Women's collection</CategoryCard>
-          <CategoryCard>Electronics</CategoryCard>
+          {categories.map((c) => {
+            return <CategoryCard key={c.ID}>{c.name}</CategoryCard>;
+          })}
         </Flex>
       </Fade>
     </Box>
--- src/components/ProductFilter/CategoryPicker.tsx
+++ src/components/ProductFilter/CategoryPicker.tsx
@@ -1,10 +1,28 @@
 import { Stack } from "@chakra-ui/layout";
 import { Radio, RadioGroup } from "@chakra-ui/radio";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+
+interface Category {
+  ID: number;
+  name: string;
+  parentCategoryName: string;
+}
 
 const CategoryPicker = () => {
   const [value, setValue] = useState("all");
+  const [categories, setCategories] = useState<Array<Category>>([]);
   //const { UpdateFilterType } = useProductListContext();
+
+  const FetchCategories = async () => {
+    const data = await fetch("https://localhost:7202/api/categories");
+    const items = await data.json();
+
+    setCategories(items);
+  };
+
+  useEffect(() => {
+    FetchCategories();
+  }, []);
 
   const HandleChange = (e: ChangeEvent<HTMLInputElement>) => {
     //UpdateFilterType(e.target.value);
@@ -17,18 +35,13 @@ const CategoryPicker = () => {
         <Radio value="all" onChange={HandleChange}>
           Show all
         </Radio>
-        <Radio value="men's clothing" onChange={HandleChange}>
-          Men's clothing
-        </Radio>
-        <Radio value="women's clothing" onChange={HandleChange}>
-          Women's clothing
-        </Radio>
-        <Radio value="jewelery" onChange={HandleChange}>
-          Jewelery
-        </Radio>
-        <Radio value="electronics" onChange={HandleChange}>
-          Electronics
-        </Radio>
+        {categories.map((c) => {
+          return (
+            <Radio key={c.ID} value={c.name} onChange={HandleChange}>
+              {c.name}
+            </Radio>
+          );
+        })}
       </Stack>
     </RadioGroup>
   );
