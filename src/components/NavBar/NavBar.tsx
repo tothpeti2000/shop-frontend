020e2faa084299db2ae2bdd83238d986f52f4a7b
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-13 14:51:24 +0100

    NavBarContainer removed

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,63 @@
 import { Spacer } from "@chakra-ui/layout";
-import React from "react";
-import NavBarContainer from "./NavBarContainer";
+import { Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import NavBrand from "./NavBrand";
 import NavLinks from "./NavLinks";
 
 const NavBar = () => {
+  const startHeight = "100px";
+  const shrinkedHeight = "70px";
+
+  const startFontSize = "lg";
+  const shrinkedFontSize = "md";
+
+  const [isShrunk, SetShrunk] = useState(false);
+
+  useEffect(() => {
+    const Handler = () => {
+      SetShrunk((isShrunk) => {
+        if (
+          !isShrunk &&
+          (document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20)
+        ) {
+          return true;
+        }
+
+        if (
+          isShrunk &&
+          document.body.scrollTop < 4 &&
+          document.documentElement.scrollTop < 4
+        ) {
+          return false;
+        }
+
+        return isShrunk;
+      });
+    };
+
+    window.addEventListener("scroll", Handler);
+
+    return () => window.removeEventListener("scroll", Handler);
+  }, []);
+
   return (
-    <NavBarContainer>
+    <Flex
+      as="nav"
+      align="center"
+      px="10%"
+      h={isShrunk ? shrinkedHeight : startHeight}
+      bgColor="black"
+      color="white"
+      sx={{ pos: "sticky", top: "0" }}
+      fontSize={isShrunk ? shrinkedFontSize : startFontSize}
+      transition="0.5s"
+      zIndex="1"
+    >
       <NavBrand />
       <Spacer />
       <NavLinks />
-    </NavBarContainer>
+    </Flex>
   );
 };
 
