c69af8f3b213b474e93b672c8f0f401ac3589bf7
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-13 21:09:19 +0100

    PriceRangePicker scaling added

--- src/components/ProductFilter/PriceRangePicker.tsx
+++ src/components/ProductFilter/PriceRangePicker.tsx
@@ -6,28 +6,47 @@ import {
   Flex,
   Text,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useProductListContext } from "../../context/ProductListContext";
 
 const PriceRangePicker = () => {
   const [range, setRange] = useState<Array<number>>([0, 100]);
+  const [maxPrice, setMaxPrice] = useState(0);
   const { UpdatePriceRange } = useProductListContext();
 
-  const ScaleUp = (values: number[]) => {
-    return [values[0] * 100, values[1] * 100];
+  const ScaleUp = (value: number) => {
+    const scale = Math.ceil(maxPrice) / 100;
+
+    return (value * scale).toFixed(2);
+  };
+
+  const ScaleUpRange = (values: number[]) => {
+    const scale = Math.ceil(maxPrice) / 100;
+
+    return [values[0] * scale, values[1] * scale];
   };
 
   const HandleChange = (value: number[]) => {
     setRange(value);
-    UpdatePriceRange(ScaleUp(range));
+    UpdatePriceRange(ScaleUpRange(value));
   };
+
+  const FetchMaxPrice = async () => {
+    // TODO: Change mock to actual fetch
+    const maxPrice = 10000;
+
+    setMaxPrice(maxPrice);
+  };
+
+  useEffect(() => {
+    FetchMaxPrice();
+  }, []);
 
   return (
     <>
       <RangeSlider
         aria-label={["min", "max"]}
         defaultValue={range}
-        value={range}
         onChange={(value) => HandleChange(value)}
       >
         <RangeSliderTrack>
@@ -43,8 +62,8 @@ const PriceRangePicker = () => {
       </Flex>
 
       <Flex justifyContent={"space-between"}>
-        <Text>{`$${range[0] * 100}`}</Text>
-        <Text>{`$${range[1] * 100}`}</Text>
+        <Text>{`$${ScaleUp(range[0])}`}</Text>
+        <Text>{`$${ScaleUp(range[1])}`}</Text>
       </Flex>
     </>
   );
