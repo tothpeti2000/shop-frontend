ec2cfb301c79bc20f02d2ea78597f40ae09571d6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-13 18:58:04 +0100

    Price range displayed

--- src/components/ProductFilter/PriceRangePicker.tsx
+++ src/components/ProductFilter/PriceRangePicker.tsx
@@ -3,18 +3,39 @@ import {
   RangeSliderTrack,
   RangeSliderFilledTrack,
   RangeSliderThumb,
+  Flex,
+  Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 const PriceRangePicker = () => {
+  const [range, setRange] = useState<Array<number>>([10, 30]);
+
   return (
-    <RangeSlider aria-label={["min", "max"]} defaultValue={[10, 30]}>
-      <RangeSliderTrack>
-        <RangeSliderFilledTrack />
-      </RangeSliderTrack>
-      <RangeSliderThumb index={0} />
-      <RangeSliderThumb index={1} />
-    </RangeSlider>
+    <>
+      <RangeSlider
+        aria-label={["min", "max"]}
+        defaultValue={range}
+        value={range}
+        onChange={(value) => setRange(value)}
+      >
+        <RangeSliderTrack>
+          <RangeSliderFilledTrack />
+        </RangeSliderTrack>
+        <RangeSliderThumb index={0} />
+        <RangeSliderThumb index={1} />
+      </RangeSlider>
+
+      <Flex justifyContent={"space-between"}>
+        <Text>From:</Text>
+        <Text>To:</Text>
+      </Flex>
+
+      <Flex justifyContent={"space-between"}>
+        <Text>{`$${range[0]}`}</Text>
+        <Text>{`$${range[1]}`}</Text>
+      </Flex>
+    </>
   );
 };
 
