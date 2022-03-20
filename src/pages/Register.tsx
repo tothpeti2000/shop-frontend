ba2258b0d646713d4a70e9f11df2a2038cb9aa01
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-20 15:01:44 +0100

    Logo added to RegisterForm

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,11 @@
 import FormContainer from "../components/Login/FormContainer";
 import RegisterForm from "../components/Login/RegisterForm";
+import { Logo } from "../Logo";
 
 const Register = () => {
   return (
     <FormContainer>
+      <Logo boxSize={"20"} mx={"auto"} mb={5} />
       <RegisterForm />
     </FormContainer>
   );
