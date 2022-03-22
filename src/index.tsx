cf6fbea0f3007209182b2211912d603cbf259f7b
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-22 10:52:16 +0100

    Routes extracted

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,15 @@
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { App } from "./App";
-import Footer from "./components/Footer/Footer";
-import NavBar from "./components/NavBar/NavBar";
-import ProductDetails from "./components/ProductDetails/ProductDetails";
-import ConfirmRegistration from "./pages/ConfirmRegistration";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import Products from "./pages/Products";
-import Register from "./pages/Register";
 import reportWebVitals from "./reportWebVitals";
+import Routing from "./Routing";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
     <ChakraProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register">
-              <Route path="" element={<Register />} />
-              <Route path="confirm" element={<ConfirmRegistration />} />
-            </Route>
-            <Route path="products" element={<Products />} />
-            <Route path="products/:id" element={<ProductDetails />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Routing />
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
