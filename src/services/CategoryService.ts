7af759ec3ff09b05738d7e55435cd715f7efff20
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-21 15:43:56 +0200

    GetTop added to CategoryService

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,26 +1,9 @@
-class CategoryService {
-  static async FetchCategories() {
-    await fetch("https://localhost:7202/api/categories");
+import { useQuery } from "react-query";
+import client from "../api/common";
+import { CategoryCover } from "../interfaces/Category";
 
-    /*const items = [
-        {
-          ID: 1,
-          name: "Construction toys",
-        },
-        {
-          ID: 2,
-          name: "LEGO",
-          parentCategoryName: "Construction toys",
-        },
-        {
-          ID: 3,
-          name: "F1 LEGO",
-          parentCategoryName: "LEGO",
-        },
-      ];*/
-  }
-
-  static async FetchTopCategories() {}
-}
-
-export default CategoryService;
+export const GetTop = () => {
+  return useQuery("categories", async () => {
+    return await client.get<CategoryCover[]>("/categories/top");
+  });
+};
