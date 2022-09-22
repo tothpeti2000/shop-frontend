e8b33aab8126cd1e47ae17638c158c6902b3f2ba
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-09-22 14:41:09 +0200

    Global bgColor added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import { theme } from "./styles/theme";
 
 const queryClient = new QueryClient();
 
