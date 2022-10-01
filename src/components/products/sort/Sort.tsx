e2385048a11f32e2a1ac4e732ca93799fc9b4b0e
tothpeti2000 <toth.peti26@gmail.com>
2022-10-01 20:33:11 +0200

    Sort modified

--- src/components/products/sort/Sort.tsx
+++ src/components/products/sort/Sort.tsx
@@ -1,13 +1,12 @@
 import { Select } from "@chakra-ui/react";
 import { ChangeEvent } from "react";
 import { useProductListContext } from "../../../context/ProductListContext";
-import { SortOption } from "../../../models/sortOption";
 
 const Sort = () => {
   const { updateSortOption } = useProductListContext();
 
   const handleSelection = (e: ChangeEvent<HTMLSelectElement>) => {
-    updateSortOption(e.target.value as SortOption);
+    updateSortOption(e.target.value);
   };
 
   return (
@@ -17,10 +16,10 @@ const Sort = () => {
       variant="filled"
       borderColor="#acacac"
     >
-      <option value={SortOption.PRICE_ASC}>Price (low to high)</option>
-      <option value={SortOption.PRICE_DESC}>Price (high to low)</option>
-      <option value={SortOption.NAME_ASC}>Name (A-Z)</option>
-      <option value={SortOption.NAME_DESC}>Name (Z-A)</option>
+      <option value="price">Price (low to high)</option>
+      <option value="price desc">Price (high to low)</option>
+      <option value="name">Name (A-Z)</option>
+      <option value="name desc">Name (Z-A)</option>
     </Select>
   );
 };
