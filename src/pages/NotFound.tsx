c3070a9a4d7ab39ab79527fcb74a252cee275257
tothpeti2000 <toth.peti26@gmail.com>
2022-10-01 13:56:09 +0200

    NotFound styling modified

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,20 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import FullScreenCenter from "../components/utils/FullScreenCenter";
 
 const NotFound = () => {
   return (
     <FullScreenCenter>
-      <Heading>404</Heading>
-      <Text>Page not found</Text>
+      <Box textAlign="center">
+        <Heading fontSize={100}>404</Heading>
+        <Text fontSize={50} mb={10}>
+          Page not found
+        </Text>
+
+        <Link to="/products">
+          <Button>Check out our products instead</Button>
+        </Link>
+      </Box>
     </FullScreenCenter>
   );
 };
