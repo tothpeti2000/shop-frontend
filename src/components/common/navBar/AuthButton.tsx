2ce686eef2fd80744fbfabe17885e2fca4637e8e
tothpeti2000 <toth.peti26@gmail.com>
2022-11-13 11:54:04 +0100

    Toast displayed after logout

--- src/components/common/navBar/AuthButton.tsx
+++ src/components/common/navBar/AuthButton.tsx
@@ -3,15 +3,18 @@ import { FaEdit, FaUserCircle } from "react-icons/fa";
 import { FiChevronDown, FiLogOut } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
 import { useUserContext } from "../../../context/UserContext";
+import useFeedback from "../../../hooks/useFeedback";
 import AppIconButton from "../../utils/AppIconButton";
 
 const AuthButton = () => {
   const { token, name, logout } = useUserContext();
+  const { showSuccess } = useFeedback();
   const navigate = useNavigate();
 
   const logoutUser = () => {
     logout();
     navigate("/login");
+    showSuccess("Successfully logged out");
   };
 
   return (
