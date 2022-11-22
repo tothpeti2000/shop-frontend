83f4df2580e36217c4156f26a8d3dc73c7b9fd27
tothpeti2000 <toth.peti26@gmail.com>
2022-11-22 22:43:52 +0100

    Unused files removed

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import { App } from "./App";
 import { CartProvider } from "./context/CartContext";
 import { SharedCartProvider } from "./context/SharedCartContext";
 import { UserProvider } from "./context/UserContext";
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
 import { theme } from "./styles/chakra-theme";
 
 const queryClient = new QueryClient();
@@ -29,13 +27,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
